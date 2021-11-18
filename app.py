d9beb29724ac1562a125de84236111b14ce955e8
LTW85 <liamtwright5@gmail.com>
2021-11-18 17:44:35 +1100

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import json
+import streamlit as st
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly, plot_components_plotly, plot_cross_validation_metric
+from datetime import datetime, timedelta
+from fbprophet.serialize import model_from_json
+from model_tuning import load_data 
+import pickle
+
+
+def main():
+
+    """
+    Loads 'pickled' parameters and perfromance metrics from tuning and cross validation. Creates and fits model,
+    then produces forecast with desiered forecast inputed via streamlit 'number_input' widget. The forecast along with
+    latest performace metrics for a 14 day horizon are displayed via streamlit widgets.
+    """
+
+    #Set title and create sidebar with 'horizon' input widget
+    st.title('ETH-USD Forecast')
+    horizon = st.sidebar.number_input(label='Forecast Horizon', 
+    min_value=1, max_value=None, value=60, step=30, help='Select forecast horizon to be displayed (default = 60 days)')
+    st.sidebar.markdown('*NOTE: the plus +/- toggles will adjust the forecast in 30 days steps. For custom horizon, type number of days in and hit enter.*')
+
+    #Load data from Yahoo Finance (function defined in model_tuning.py), as well as 'pickled' parameters and cross-validation performance metrics
+    data=load_data()
+    params_in = open('tuned_params.pickle', 'rb')
+    params = pickle.load(params_in)
+    outlook_in = open('outlook.pickle', 'rb')
+    outlook = pickle.load(outlook_in)
+    outlook = outlook.astype({'Horizon': 'str'})
+
+    #Build and fit Prohpet model with tuned 'changepoint_prior_scale' and 'seasonality_prior_scale' parameters 
+    model=Prophet(
+    seasonality_mode="multiplicative",
+    yearly_seasonality=True,
+    interval_width = 0.95,
+    changepoint_prior_scale=params['changepoint_prior_scale'][0],
+    seasonality_prior_scale=params['seasonality_prior_scale'][0]
+    )
+
+    model.fit(data)
+
+    #Produce forecast and forecast plot employing the user-defined horizon
+    future=model.make_future_dataframe(periods=horizon)
+    forecast=model.predict(future)
+    forecast_data = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+    forecast_plot=plot_plotly(model, forecast_data, xlabel='Date', ylabel='Price (USD)')
+
+    #Display forecast plot using streamlit widget
+    st.markdown('### Forecast Plot')
+    st.markdown('*Data scource: Yahoo Finance*')  
+    st.plotly_chart(forecast_plot, use_container_width=True)
+
+    #Display cross-validation performance metrics using streamlit widget
+    st.markdown('### Cross-validation performance metrics for 14 day horizon')
+    st.dataframe(outlook)
+    
+#Execute 'main' function
+if __name__ == '__main__':
+    main()
+
+
---
+++ model_tuning.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import itertools
+import streamlit as st
+import yfinance as yf
+import pickle
+from fbprophet import Prophet
+from datetime import datetime, timedelta
+from fbprophet.diagnostics import cross_validation, performance_metrics
+
+
+def load_data():
+    """
+    Pull data from Yahoo Finance ranging from '2016-01-01' to the close price from the previous day.
+    Data will cleaned and and organised in a way that it suitable for the Prophet model.
+    """
+    #Pull data using the yfinacne library
+    start_date='2016-01-01'
+    end_date=datetime.today() - timedelta(1)
+    end_date=datetime.strftime(end_date, '%Y-%m-%d')
+    yf_data=yf.download('ETH-USD',start_date, end_date)
+
+    #Clean data and fill in missing values using a 'forward fill' method
+    yf_data=yf_data.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1)
+    pd.date_range(start=start_date, end=end_date ).difference(yf_data.index)
+    new_date_range=pd.date_range(start=start_date, end=end_date, freq="D")
+    yf_data=yf_data.reindex(new_date_range, method='ffill')
+
+    #Organise data in a manner that suitable for use with Prophet
+    prophet_df=yf_data.reset_index(level=0)
+    prophet_df=prophet_df.rename({'index': 'ds', 'Close': 'y'}, axis=1)
+
+    return prophet_df
+
+def tune():
+    """
+    Tune a prophet model using pre-defined cutoff periods. Returns a dataframe with a single row containing
+    optimal 'changepoint_prior_scale' value and 'seasonality_prior_scale' value.
+    """
+    #Define cutoff periods and parameters values to be searched
+    cutoffs=pd.to_datetime(['2018-01-01', '2018-04-01', '2019-01-01', '2020-01-01'])
+    param_grid={  
+        'changepoint_prior_scale': [0.001, 0.01, 0.1, 0.5],
+        'seasonality_prior_scale': [0.01, 0.1, 1.0, 10.0],
+    }
+
+    #Create list containing all parameter combinations
+    all_params=[dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())]
+    mae=[]  # Store the RMSEs for each params here
+
+    #Perfrom grid-search using mean absolute error (mae) as evaluation metric
+    for params in all_params:
+        m = Prophet(**params, seasonality_mode="multiplicative").fit(load_data()) 
+        df_cv = cross_validation(m, cutoffs=cutoffs, horizon='60 days', parallel="processes")
+        df_p = performance_metrics(df_cv, rolling_window=1)
+        mae.append(df_p['mae'].values[0])
+
+    #Create dataframe of results and return best perfroming parameters
+    tuning_results=pd.DataFrame(all_params)
+    tuning_results['mae']=mae
+    tuning_results = tuning_results.sort_values(['mae'])[:1]
+    
+    return tuning_results
+
+def cross_val(m):
+   """
+   Perform cross-validation and return dataframe of perfrormance metrics
+   """ 
+   #Perform cross-validation (resulting in 46 model fits) and produce performance metrics
+   df_cv=cross_validation(m, initial='730 days', period='30 days', horizon = '60 days')
+   df_p=performance_metrics(df_cv)
+
+   return df_p
+
+def get_outlook(df):
+    """
+    Convert 'mae' and 'mape' into user-friendly evaluation metrics. Returns a dataframe with horizon, average +/- dollars, and average accuracy  
+    """
+    #Extract metrics of interest ('mae' and 'mape')
+    forecast_outlook = df[['horizon', 'mae', 'mape']][:9]
+    forecast_outlook = forecast_outlook.rename({'mae': '+/- Dollars (USD)', 'horizon': 'Horizon'}, axis = 1)
+
+    #Create 'accuracy' metrics using 'mape' and produce final dataframe
+    forecast_outlook['Accuracy (%)'] = forecast_outlook.apply(lambda row: 100 - row['mape']*100, axis = 1)
+    forecast_outlook = forecast_outlook.drop('mape', axis = 1)
+    forecast_outlook = forecast_outlook.round(2)
+
+    return forecast_outlook
+
+def main():
+    """
+    Perform grid-search to find best values for hyper-parameters 'changepoint_prior_scale' and 'seasonality_prior_scale'.
+    Create model using tuned parameters and perform cross-validation to aquire performance metrics. Tuned parameters values 
+    and performance metrics are then 'pickled'.
+    """
+    #Get tuned parameters and load data for model
+    tuned_params = tune()
+    data=load_data()
+
+    #Create Prophet model object and fit to data
+    model=Prophet(
+    seasonality_mode="multiplicative",
+    yearly_seasonality=True,
+    interval_width=0.95,
+    changepoint_prior_scale=tuned_params['changepoint_prior_scale'][0],
+    seasonality_prior_scale=tuned_params['seasonality_prior_scale'][0]
+    )
+
+    model.fit(data)
+
+    #Get perfromacne metrics from cross-validation
+    outlook_res = get_outlook(cross_val(model))
+
+    #Pickle tuned parameters
+    pickle_params = open('tuned_params.pickle', 'wb')
+    pickle.dump(tuned_params, pickle_params)
+    pickle_params.close()
+
+    #Pickle cross-validation performance metrics
+    pickle_outlook = open('outlook.pickle', 'wb')
+    pickle.dump(outlook_res, pickle_outlook)
+    pickle_outlook.close()
+
+#Execute 'main' function
+if __name__ == '__main__':
+    main()
+
+
+
+
+
