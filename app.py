ccb76d1a82704d42038bb3b8206a9e6a17a44b47
LTW85 <liamtwright5@gmail.com>
2021-11-19 09:27:20 +1100

    Update forecast

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def main():
     future=model.make_future_dataframe(periods=horizon)
     forecast=model.predict(future)
     forecast_data = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
-    forecast_plot=plot_plotly(model, forecast_data, xlabel='Date', ylabel='Price (USD)')
+    forecast_plot=plot_plotly(model, forecast, xlabel='Date', ylabel='Price (USD)')
 
     #Display forecast plot using streamlit widget
     st.markdown('### Forecast Plot')
