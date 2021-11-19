b9377f4709b611d379c2a97f7044a8d03ba6e9f5
LTW85 <liamtwright5@gmail.com>
2021-11-19 14:38:37 +1100

    Update streamlit interface

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
     st.sidebar.header('Forecast Horizon')
     horizon = st.sidebar.number_input(label='Input the number of days to be forecast', 
     min_value=1, max_value=None, value=60, step=30, help='Select forecast horizon to be displayed (default = 60 days)')
-    st.sidebar.markdown('*NOTE: the +/- toggles will adjust the forecast in 30 days steps. For custom horizon, type number of days in and hit enter.*')
+    st.sidebar.markdown('*NOTE: the +/- toggles will adjust the forecast in 30 day steps. For custom horizon, type number of days in and hit enter.*')
 
     #Load data from Yahoo Finance (function defined in model_tuning.py), as well as 'pickled' parameters and cross-validation performance metrics
     data=load_data()
