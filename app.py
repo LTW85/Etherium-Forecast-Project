a132859d0e5d4094927af3f987cac71685307e60
LTW85 <liamtwright5@gmail.com>
2021-11-21 10:23:57 +1100

    Add Readme - adjust changepoints

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def main():
     seasonality_mode="multiplicative",
     yearly_seasonality=True,
     interval_width = 0.95,
+    changepoint_range = 0.9,
     changepoint_prior_scale=params['changepoint_prior_scale'][0],
     seasonality_prior_scale=params['seasonality_prior_scale'][0]
     )
