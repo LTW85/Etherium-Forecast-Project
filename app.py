29f2c9848357490ffc0a062f16b8f1fe0061e1c2
LTW85 <liamtwright5@gmail.com>
2021-11-21 16:34:08 +1100

    Update Streamlit interface

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def main():
     min_value=1, max_value=None, value=60, step=30, help='Select forecast horizon to be displayed (default = 60 days)')
     st.sidebar.markdown('*NOTE: the +/- toggles will adjust the forecast in 30 day steps. For custom horizon, type number of days in and hit enter.*')
     st.sidebar.header('General Information')
-    st.sidebar.markdown('New data collected daily. Model tuning is also conducted daily to ensure the forecast is in-line with comtemporary market conditions.' 
-    'Currently cross-validation suggests a maximum forecast horizon of 10-14 is most suitable in terms of accuracy. Cross-validation performance metrics for a 6 - 14 day horizon are also updated daily')
+    st.sidebar.markdown('New data collected daily. Model tuning is also conducted daily to ensure the forecast is in-line with contemporary market conditions. \n' 
+    'Currently, cross-validation suggests a maximum forecast horizon of 10-14 is most suitable in terms of accuracy. Cross-validation performance metrics for a 6 - 14 day horizon are also updated daily.')
 
     #Load data from Yahoo Finance (function defined in model_tuning.py), as well as 'pickled' parameters and cross-validation performance metrics
     data=load_data()
